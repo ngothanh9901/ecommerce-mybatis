750b9a6815083fe3e4e749c428d6a9133c38b6a3
ngoducthanh <thanh.ngoduc@vti.com.vn>
2023-07-12 09:41:04 +0700

    setup swagger

---
+++ src/main/java/com/example/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                "/configuration/security", "/swagger-ui.html", "/webjars/**","/swagger-ui/*");
+    }
+}
